645a963d78c78797071aa2b38349e7cc0e55ad8e
Roeland Matthijssens <fulgens.ailurus@gmail.com>
2016-11-27 00:37:37 +0100

    added colors
    also added a bug where we calculated the z values wrong

--- sketch.js
+++ sketch.js
@@ -1,19 +1,21 @@
 var width, height, maxIterations, infinityThreshold;
-var X, Y, size;
+var X, Y, size, zoomslider;
 function setup() {
-    width = 500;
-    height = 500;
-    maxIterations = 255;
+    width = 250;
+    height = 250;
+    maxIterations = 1000;
     infinityThreshold = 20;
     X = 0;
     Y = 0;
     size = 4;
     createCanvas(width, height);
     pixelDensity(1);
+    zoomslider = createSlider(0, 1, 1, 0.01);
 }
 
 function draw(){
     loadPixels();
+    size = size*zoomslider.value();
     setBackground(51, 51, 51, 255);
     eachPixelDo(calculateMandelbroth);
     updatePixels();
@@ -29,37 +31,54 @@ function calculateMandelbroth(pixel, x, y){
     // z0 = 0 + c
     // z1 = c^2 + c
     // z2 = (c^2 + c)^2 + c
-    var ca = map(x, 0, width, X-size, X+size);
-    var cb = map(y, 0, height, Y-size, Y+size);
-    var a = ca;
-    var b = cb;
+    var a = map(x, 0, width, X-size, X+size);
+    var b = map(y, 0, height, Y-size, Y+size);
+    var ca = a;
+    var cb = b;
     var iteratedAmount = 0;
 
     while (iteratedAmount < maxIterations){
         var za = a*a - b*b;
         var zb = 2*a*b;
-        if(za+zb > infinityThreshold){
-            break;
-        }
         a = za + ca;
         b = zb + cb;
+        if(a*a+b*b > infinityThreshold){
+            break;
+        }
         iteratedAmount++;
     }
-    var c;
-    if(iteratedAmount == maxIterations){
-        c = 0;
-    } else {
-        //c = map(iteratedAmount, 0, maxIterations, 0, 255);
-        c = map(iteratedAmount, 0, maxIterations, 0, 1);
-        c = map(sqrt(c), 0, 1, 0, 255);
-    }
-
-    colorPixel(pixel, [c, c, c, 255]);
+    var c = chooseColor(iteratedAmount);
+    colorPixel.apply(null, [pixel, c]);
 }
 
-function setBackground(red, green, blue, alpha){
+function chooseColor(n){
+    if (n < maxIterations && n > 0) {
+        var i = n % 16;
+        var mapping = [];
+        mapping[0]  = [66,  30,  15 ];
+        mapping[1]  = [25,  7,   26 ];
+        mapping[2]  = [9,   1,   47 ];
+        mapping[3]  = [4,   4,   73 ];
+        mapping[4]  = [0,   7,   100];
+        mapping[5]  = [12,  44,  138];
+        mapping[6]  = [24,  82,  177];
+        mapping[7]  = [57,  125, 209];
+        mapping[8]  = [134, 181, 229];
+        mapping[9]  = [211, 236, 248];
+        mapping[10] = [241, 233, 191];
+        mapping[11] = [248, 201, 95 ];
+        mapping[12] = [255, 170, 0  ];
+        mapping[13] = [204, 128, 0  ];
+        mapping[14] = [153, 87,  0  ];
+        mapping[15] = [106, 52,  3  ];
+        return mapping[i];
+    }
+    else return [0, 0, 0];
+}
+
+function setBackground(red, green, blue){
     eachPixelDo(function(pixel){
-        colorPixel(pixel, [51,51,51,255]);
+        colorPixel(pixel, [51,51,51]);
     })
 }
 
@@ -76,11 +95,10 @@ function colorPixel(pixel, color){
     var red = color[0];
     var green = color[1];
     var blue = color[2];
-    var alpha = color[3];
     pixels[pixel]   = red;
     pixels[pixel+1] = green;
     pixels[pixel+2] = blue;
-    pixels[pixel+3] = alpha;
+    pixels[pixel+3] = 255;
 }
 
 function mouseWheel(event) {
